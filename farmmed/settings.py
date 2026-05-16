eee834a18c33dc5ec61896c921ec8c688a353d38
Mir Mohammad Sajjadul Islam <127872800+mirsajjad1@users.noreply.github.com>
2026-05-16 21:01:56 +0600

    Update ALLOWED_HOSTS

--- farmmed/settings.py
+++ farmmed/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oaqk&jgs1)%98weop^sj!ko4a1h26!)bbp%8e9047+ssf6=7@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
